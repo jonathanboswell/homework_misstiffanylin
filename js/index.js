0de8bde8b472ab91db0ea9af344094fb75a70c98
misstiffanylin <tlin625@gmail.com>
2016-11-11 22:35:53 -0800

    Tiffany's Final Project

---
+++ js/index.js
@@ -0,0 +1,55 @@
+
+
+$(document).ready(
+    function(){
+
+        $('#emailForm').submit(function(submitEvent){
+            submitEvent.preventDefault();
+         })
+
+        var genre = ['Chill House', 'Deep House', 'Trap/Hip Hop', 'Progressive House'];
+            genre.forEach(function(value, key){
+            $('#playlist').append('<button class="btn btn-default" value=' + key + '>' + value +'</button> ');
+        });
+
+        $("#playlist .btn").click ( function() {
+            var button = $(this);
+            $("#playlist .btn").removeClass("black");
+            button.addClass("black");
+        });
+
+        $("ul.nav li a").click( function() {
+            var button = $(this);
+            $("ul.nav li a").removeClass("black");
+            button.addClass("black");
+        });
+
+        $("#playlist button:nth-child(2)").click(function(){
+            $(".list").addClass('hiddenPlaylist');
+            $("#chill").removeClass('hiddenPlaylist');
+        });
+    
+   
+        $("#playlist button:nth-child(3)").click(function(){
+            $(".list").addClass('hiddenPlaylist');
+            $("#deep").removeClass('hiddenPlaylist')
+        });
+
+
+    
+        $("#playlist button:nth-child(4)").click(function(){
+            $(".list").addClass('hiddenPlaylist');
+            $("#trapHipHop").removeClass('hiddenPlaylist')
+        });
+    
+
+    
+        $("#playlist button:nth-child(5)").click(function(){
+            $(".list").addClass('hiddenPlaylist');
+            $("#progressive").removeClass('hiddenPlaylist')
+        });
+
+    }
+);
+
+
